634ed2ddc09ce7177a5e8b469acc08ddbd798e2f
Khamvang <108178100+Khamvang@users.noreply.github.com>
2022-11-28 13:26:39 +0700

    Update add_new_contact_file.sql

--- add_new_contact_file.sql
+++ add_new_contact_file.sql
@@ -143,3 +143,52 @@ where id in (select id from removed_duplicate where `time` >= '2022-11-28'); -- 
 
 -- 14) check and import date from contact_numbers to contact_numbers_to_lcc
 select distinct province_eng from contact_numbers where file_id >= 1064;
+
+insert into contact_numbers_to_lcc (`id`,`file_id`,`contact_no`,`name`,`province_eng`,`province_laos`,`district_eng`,`district_laos`,`village`,`type`,`maker`,`model`,`year`,`remark_1`,`remark_2`,`remark_3`,`branch_name`,`status`,`file_no`,`date_received`,`date_updated`,`pbxcdr_time`)
+select cn.`id`,cn.`file_id`,cn.`contact_no`,
+	case when cn.`name` = '' then null else cn.`name` end `name`, 
+	case when cn.`province_eng` = '' then null else cn.`province_eng` end `province_eng`,
+	case when cn.`province_laos` = '' then null else cn.`province_laos` end `province_laos`,
+	case when cn.`district_eng` = '' then null else cn.`district_eng` end `district_eng`,
+	case when cn.`district_laos` = '' then null else cn.`district_laos` end `district_laos`,
+	case when cn.`village` = '' then null else cn.`village` end `village`,
+	cn.`type`, 
+	case when cn.`maker` = '' then null else cn.`maker` end `maker`,
+	case when cn.`model` = '' then null else cn.`model` end `model`,
+	case when cn.`year` = '' then null else cn.`year` end `year`,
+	null `remark_1`,null `remark_2`,null `remark_3`,
+	case when cn.province_eng = 'ATTAPUE' then 'Attapue'
+		when cn.province_eng = 'BORKEO' then 'Bokeo'
+		when cn.province_eng = 'BORLIKHAMXAY' then 'Paksan'
+		when cn.province_eng = 'CHAMPASACK' then 'Pakse'
+		when cn.province_eng = 'HUAPHAN' then 'Houaphan'
+		when cn.province_eng = 'KHAMMOUAN' then 'Thakek'
+		when cn.province_eng = 'LUANG PRABANG' then 'Luangprabang'
+		when cn.province_eng = 'LUANGNAMTHA' then 'Luangprabang'
+		when cn.province_eng = 'OUDOMXAY' then 'Oudomxay'
+		when cn.province_eng = 'PHONGSALY' then 'Oudomxay'
+		when cn.province_eng = 'SALAVANH' then 'Salavan'
+		when cn.province_eng = 'SAVANNAKHET' then 'Savannakhet'
+		when cn.province_eng = 'VIENTIANE CAPITAL' then 'Head office'
+		when cn.province_eng = 'VIENTIANE PROVINCE' then 'Vientiane province'
+		when cn.province_eng = 'XAYABOULY' then 'Xainyabuli'
+		when cn.province_eng = 'XAYSOMBOUN' then 'Xiengkhouang'
+		when cn.province_eng = 'XEKONG' then 'Attapue'
+		when cn.province_eng = 'XIENGKHUANG' then 'Xiengkhouang'
+		when cn.province_eng = '' then fd.branch_name 
+		when cn.province_eng is null then fd.branch_name 
+		else null 
+	end `branch_name` ,
+	null `status`,fd.`file_no`,fd.`date_received`,date(now()) `date_updated`, 0 `pbxcdr_time`
+from contact_numbers cn left join file_details fd on (fd.id = cn.file_id)
+where CONCAT(LENGTH(cn.contact_no), left( cn.contact_no, 5)) in ('1190302','1190304','1190305','1190307','1190309','1290202','1290205','1290207','1290208','1290209')
+	and file_id >= 1064; -- done <= 1064
+
+select `type` , count(*) from contact_numbers_to_lcc cntl where file_id >= 1064 group by `type` ;
+
+-- 15) check and remove duplicate Delete from all unique where id = id in table removed duplicate 
+-- **** insert the old data to keep in table temp_merge_data for doing merge after 
+delete from temp_merge_data ;
+
+
+
